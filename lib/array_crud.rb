ea2d900d35d4b02e1cc9f61c1d62e2cffc865ee9
Erika Davis <davis2erika@gmail.com>
2019-04-25 01:36:26 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  empty_array = Array.new
 end
 
 def create_an_array
-  
+  favorite_manga = ["tokyo ghoul", "one punch man", "wotakoi", "promised neverland"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_manga = ["tokyo ghoul", "one punch man", "wotakoi", "promised neverland"]
+  favorite_manga << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  favorite_manga = ["tokyo ghoul", "one punch man", "wotakoi", "promised neverland"]
+  favorite_manga.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  favorite_manga = ["tokyo ghoul", "one punch man", "wotakoi", "promised neverland", "arrays!"]
+  favorite_manga.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  favorite_manga = ["wow", "tokyo ghoul", "one punch man", "wotakoi", "promised neverland"]
+  favorite_manga.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  favorite_manga = ["tokyo ghoul", "one punch man", "wotakoi", "am", "promised neverland"]
+  favorite_manga[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  favorite_manga = ["wow", "tokyo ghoul", "one punch man", "wotakoi", "promised neverland"]
+  favorite_manga.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  favorite_manga = ["tokyo ghoul", "one punch man", "wotakoi", "promised neverland", "arrays!"]
+  favorite_manga.last
 end
