c79d4f3abbed7be22916c33f8a389d029c4f32c7
Erika Davis <davis2erika@gmail.com>
2019-04-25 01:18:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  favorite_manga = ["wow", "tokyo ghoul", "one punch man", "wotakoi", "promised neverland"]
+  favorite_manga.shift
 end
 
 def retrieve_element_from_index(array, index_number)
